7acecc243d457875c3025b520552e545ff4ae228
Nganthoiba <leecba@gmail.com>
2025-08-17 19:10:55 +0530

    adding notifications

---
+++ database/migrations/2025_08_17_124823_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id('notification_id');
+            $table->string('document_type')->nullable();
+            $table->string('document_caption');
+            $table->string('document_link')->nullable();
+            $table->date('validity');
+            $table->timestamp('is_deleted')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
