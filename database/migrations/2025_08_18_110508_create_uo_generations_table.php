e9c0c26c8a2aebf7d9aee8e4e2bcc46d7a8a7e65
Nganthoiba <leecba@gmail.com>
2025-08-26 20:00:09 +0530

    add one more field for storing storage path for signed document.

--- database/migrations/2025_08_18_110508_create_uo_generations_table.php
+++ database/migrations/2025_08_18_110508_create_uo_generations_table.php
@@ -29,6 +29,7 @@ return new class extends Migration
             $table->string('alloted_dsg_srno', 20)->comment('Post id selected or requested by the applicant');
             $table->string('alloted_dsg_desc', 255);
             $table->string('alloted_group_code', 20);
+            $table->string('signed_proforma_doc', 255)->nullable()->comment('Storage path for signed proforma document after uo generation');
             $table->unsignedBigInteger('signing_authority')->nullable();
 
             $table->timestamps(); // created_at, updated_at
