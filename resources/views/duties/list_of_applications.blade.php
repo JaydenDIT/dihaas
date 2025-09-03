4e735aa053521a18a16995b6f367b228c7c57a69
JaydenDIT <145315991+JaydenDIT@users.noreply.github.com>
2025-09-03 12:05:47 +0530

    dept api

--- resources/views/duties/list_of_applications.blade.php
+++ resources/views/duties/list_of_applications.blade.php
@@ -59,7 +59,7 @@
                 <div class="col-md-6">
                     <select class="form-select" aria-label="Default select example" id="dept_id" name="dept_id">
                         <option value="" selected>All Department</option>
-                        @foreach ($departments['field_dept'] as $dept)
+                        @foreach ($departments as $dept)
                             <option value="{{ $dept['field_dept_cd'] }}">{{ $dept['field_dept_desc'] }}</option>
                         @endforeach
                     </select>
