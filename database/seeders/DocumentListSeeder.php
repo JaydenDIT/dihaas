fef2b024c7a34d4bec1c7db678fc83d0e4a421c4
Nganthoiba <leecba@gmail.com>
2025-09-04 07:51:55 +0530

    seeder correction

--- database/seeders/DocumentListSeeder.php
+++ database/seeders/DocumentListSeeder.php
@@ -10,32 +10,30 @@ class DocumentListSeeder extends Seeder
     public function run(): void
     {
         $documents = [
-            ['Death Certificate', 'compulsory', 'pdf'],
-            ['Termination Order of Deceased', 'compulsory', 'pdf'],
-            ['Age Proof Certificate (Birth Certificate or H.S.L.C)', 'compulsory', 'pdf'],
-            ['Education Qualification Certificate', 'compulsory', 'pdf'],
-            ['Additional Qualification Certificate', 'optional', 'pdf'],
-            ['Lists of family certificate indicating DoB/Sex from SDO', 'compulsory', 'pdf'],
-            ['No. of employee in the family Certificate from SDC/SDO', 'compulsory', 'pdf'],
-            ['Income Certificate from SOC/SDO', 'compulsory', 'pdf'],
-            ['NOC from wife/husband if applicant is son/daughter (in the form of Court affidavit)', 'compulsory', 'pdf'],
-            ['Affidavit from all eligible children if applicant is a son/daughter of deceased employee (NOC)', 'compulsory', 'pdf'],
-            ['Undertaking from the applicant to look after the children/dependent (Affidavit)', 'compulsory', 'pdf'],
-            ['Jamabandi Land Valuation Certificate', 'compulsory', 'pdf'],
-            ['Caste/Tribe Certificate', 'caste', 'pdf'],
-            ['Electoral Roll', 'compulsory', 'pdf'],
-            ['Police Report (if dead on duty)', 'dead_on_duty', 'pdf'],
-            ['Physically Handicapped Certificate (if any)', 'handicapped', 'pdf'],
-            ['Passport Photo', 'compulsory', 'image'],
-            ['Others', 'optional', 'pdf'],
+            ['document_name' => 'Death Certificate', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Termination Order of Deceased', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Age Proof Certificate (Birth Certificate or H.S.L.C)', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Education Qualification Certificate', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Additional Qualification Certificate', 'document_criteria' => 'optional', 'document_type' => 'pdf'],
+            ['document_name' => 'Lists of family certificate indicating DoB/Sex from SDO', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'No. of employee in the family Certificate from SDC/SDO', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Income Certificate from SOC/SDO', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'NOC from wife/husband if applicant is son/daughter (in the form of Court affidavit)', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Affidavit from all eligible children if applicant is a son/daughter of deceased employee (NOC)', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Undertaking from the applicant to look after the children/dependent (Affidavit)', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Jamabandi Land Valuation Certificate', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Caste/Tribe Certificate', 'document_criteria' => 'caste', 'document_type' => 'pdf'],
+            ['document_name' => 'Electoral Roll', 'document_criteria' => 'compulsory', 'document_type' => 'pdf'],
+            ['document_name' => 'Police Report (if dead on duty)', 'document_criteria' => 'dead_on_duty', 'document_type' => 'pdf'],
+            ['document_name' => 'Physically Handicapped Certificate (if any)', 'document_criteria' => 'handicapped', 'document_type' => 'pdf'],
+            ['document_name' => 'Passport Photo', 'document_criteria' => 'compulsory', 'document_type' => 'image'],
+            ['document_name' => 'Others', 'document_criteria' => 'optional', 'document_type' => 'pdf'],
         ];
 
-        foreach ($documents as $doc) {
-            DocumentList::create([
-                'document_name'     => $doc[0],
-                'document_criteria' => $doc[1],
-                'document_type'     => $doc[2]
-            ]);
-        }
+        DocumentList::upsert(
+            $documents,
+            ['document_name'], // unique
+            ['document_criteria', 'document_type'] // update these if duplicate
+        );
     }
 }
