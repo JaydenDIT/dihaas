1f5969358c6639c50ff6f3ca4956067a2da82411
Nganthoiba <leecba@gmail.com>
2025-09-04 13:14:54 +0530

    Seeder update

--- database/seeders/CasteSeeder.php
+++ database/seeders/CasteSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Caste;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,12 +16,26 @@ class CasteSeeder extends Seeder
     public function run(): void
     {
         //
-        DB::insert("insert into castes (caste_name) values  
+        /* DB::insert("insert into castes (caste_name) values  
                     ('OBCM'), 
                     ('OBC-NCL'), 
                     ('SC'), 
                     ('ST'), 
                     ('General')
-        ");
+        "); */
+
+        $castes = [
+            ['caste_name' => 'OBCM'],
+            ['caste_name' => 'OBC-NCL'],
+            ['caste_name' => 'SC'],
+            ['caste_name' => 'ST'],
+            ['caste_name' => 'General'],
+        ];
+
+        Caste::upsert(
+            $castes,
+            ['caste_name'], // unique
+            [] // nothing to update
+        );
     }
 }
--- database/seeders/RelationshipsSeeder.php
+++ database/seeders/RelationshipsSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Relationship;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,13 +16,26 @@ class RelationshipsSeeder extends Seeder
     public function run(): void
     {
         //
-        DB::insert("insert into relationships (relationship_name) values  
+        /* DB::insert("insert into relationships (relationship_name) values  
                 ('Wife'),
                 ('Husband'),
                 ('Son'),
                 ('Daughter'),
                 ('Unmarried Sister'),
                 ('Unmaried Brother')
-                ");
+                "); */
+        $relationships = [
+            ['relationship_name' => 'Wife'],
+            ['relationship_name' => 'Husband'],
+            ['relationship_name' => 'Son'],
+            ['relationship_name' => 'Daughter'],
+            ['relationship_name' => 'Unmarried Sister'],
+            ['relationship_name' => 'Unmarried Brother'],
+        ];
+        Relationship::upsert(
+            $relationships,
+            ['relationship_name'],
+            []
+        );
     }
 }
