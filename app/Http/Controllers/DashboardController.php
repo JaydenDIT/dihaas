c67270aec7645f1c06135a2271292a9d017448e6
Nganthoiba <leecba@gmail.com>
2025-09-04 10:44:25 +0530

    counts corrected

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,19 +21,25 @@ class DashboardController extends Controller
             $officialCount = User::whereNotIn('role_id', Role::where('role_group', 'citizen')->pluck('role_id'))->count();
             $citizenCount = User::whereIn('role_id', Role::where('role_group', 'citizen')->pluck('role_id'))->count();
 
-            //Finding total applications submitted
-            $totalAppCount = Proforma::whereNotIn('mini_sequence', [
-                'step1-completed',
-                'step2-completed',
-                //'step3-completed',
-            ])->count();
+            //Finding total applications submitted          
+
+            $totalAppCount = Proforma::where(function ($query) {
+                $query->whereNotIn('mini_sequence', [
+                    'step1-completed',
+                    'step2-completed',
+                    'step3-completed',
+                ])->orWhereNull('mini_sequence');
+            })->count();
 
             //Finding number of applications pending
-            $pendingAppCount = Proforma::whereNotIn('mini_sequence', [
-                'step1-completed',
-                'step2-completed',
-                'step3-completed',
-            ])->where('proforma_status', '!=', 'completed')->count();
+            $pendingAppCount = Proforma::where(function ($query) {
+                $query->whereNotIn('mini_sequence', [
+                    'step1-completed',
+                    'step2-completed',
+                    'step3-completed',
+                ])->orWhereNull('mini_sequence');
+            })->where('proforma_status', '!=', 'completed')->count();
+
 
             //Finding number of applications pending
             $completedAppCount = Proforma::where('proforma_status', '=', 'completed')->count();
