3cda86bdd6df4ce978e13f02295165b951f9a7ed
JaydenDIT <145315991+JaydenDIT@users.noreply.github.com>
2025-09-09 10:48:41 +0530

    task update

--- config/tasks_file.php
+++ config/tasks_file.php
@@ -4,9 +4,9 @@ return [
     'verify_and_forward' => 'Verification and forward',
     'verify_physical_copy' => 'Verification of Documents by Department',
     'client_form_submission' => 'Citizen Form Submission',
-    'uo_file_submission' => "Submit File for UO Form Fill Up",
-    'uo_approval' => 'UO Approval',
-    'uo_file_approval' => 'Approval of UO file',
-    'uo_formfillup' => 'UO Form Fill UP',
-    'uo_form_generation' => 'Generate UO Form',
+    'uo_file_submission' => "Submit File for UO Form Fill Up", //DPassistant forward to nodal with a file and remark
+    //'uo_approval' => 'UO Approval',
+    'uo_file_approval' => 'Approval of UO file', //Nodal to Approve and forward to DPassistant
+    'uo_formfillup' => 'UO Form Fill UP', //DPassistant to fillup UO Form and forward to a Nodal
+    'uo_form_generation' => 'Generate UO Form', //Nodal to generate UO
 ];
